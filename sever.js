c42779e3b8c4da8dcf3ddf21bd602be9451fbbb2
ALEXANDER <msumba83@gmail.com>
2023-03-27 15:09:23 +0200

    server side

---
+++ sever.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+const port = 5000;
+const state = "is running";
+const package = "nodemon";
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+
+app.listen(port, () => {
+  console.log(`AJ-TOURS server ${state} using ${package}  on port ${port}`);
+});
